3e6a009393040a1f085e57d99005aaab336a42d3
King David <davidwiers@davids-mbp.lan>
2022-02-21 15:09:38 -0600

    adding files before the written part of the assignment

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -67,4 +67,64 @@ ORDER BY count(ur.emp_no) DESC;
 -- dropping table to resolve errors
 Drop Table Retiring_Titles;
 -- viewing table to see if it worked correctly
-Select * from Retiring_Titles;
+Select * from dept_info;
+
+
+-- Deliverable 2 for the challenge;
+-- -- retrieving the required columns from the resources
+SELECT DISTINCT ON (emp_no) em.emp_no,
+em.first_name,
+em.last_name,
+em.birth_date
+dm.from_date,
+dm.to_date, 
+	ti.titles
+INTO mentorship_eligibility
+FROM employees, dept_emp, titles AS  em, dm, ti
+WHERE em.to_date = ('9999-01-01') AND (em.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+-- DISTINCT ON to get the first occurence of the employee number for each set of rows defined by the on clause
+-- filter to_date to get only current employees and then filter the birth_date to get employees whose birth dates are between January 1965 and Dec 31 1965
+ORDER BY em.emp_no;
+
+-- didnt need to end up creating this table because i created it from the statements below that used the decide on script too
+-- CREATE TABLE mentorship_eligibility(
+-- 	emp_no INT not NULL,
+-- 	first_name varchar NOT NULL,
+-- 	last_name varchar  NOT NULL,
+-- 	birth_date date  NOT NULL,
+-- 	from_date date  NOT NULL,
+-- 	to_date date  NOT NULL,
+-- 	title varchar  NOT NULL
+-- 	)
+	
+SELECT * FROM mentorship_eligibility;	
+DROP TABLE mentorship_eligibility;	
+
+
+-- the winning code right here!! 
+SELECT DISTINCT ON (emp_no)	
+	em.emp_no,
+		em.first_name,
+		em.last_name,
+		em.birth_date,
+	dm.from_date,
+	dm.to_date, 
+ti.title
+INTO mentorship_eligibility
+FROM employees AS  em
+Right JOIN dept_emp AS dm
+ON (em.emp_no = dm.emp_no)
+RIGHT JOIN titles as ti
+ON (em.emp_no = ti.emp_no)
+WHERE ti.to_date = ('9999-01-01') AND (em.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+-- DISTINCT ON to get the first occurence of the employee number for each set of rows defined by the on clause
+-- filter to_date to get only current employees and then filter the birth_date to get employees whose birth dates are between January 1965 and Dec 31 1965
+ORDER BY em.emp_no;
+
+SELECT * from mentorship_eligibility;
+
+
+
+
+
+
